1b674696a48926b262785672cddb9f4ae430f16e
jfalkson <jfalkson@gmail.com>
2014-04-19 22:04:58 -0400

    User table with devise added

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  #making a call to devise importing the below modules
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
