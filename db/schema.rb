80241ec00b2ce678f7d423ebec7cd38c147cfecf
jfalkson <jfalkson@gmail.com>
2014-04-23 22:33:47 -0400

    adding table for user daily (calorie intake, exercise calories, etc)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140420020019) do
+ActiveRecord::Schema.define(version: 20140424022941) do
+
+  create_table "userdailydata", force: true do |t|
+    t.string   "user_id"
+    t.integer  "calories_consumed"
+    t.integer  "calories_exercised"
+    t.datetime "date"
+    t.integer  "weight"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
