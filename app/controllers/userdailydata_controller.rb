8d008c3af013a067ed1a2cda764584faa9e9f551
jfalkson <jfalkson@gmail.com>
2014-05-04 23:38:34 -0400

    data only shows up for specific user now, also datetime is formatted better. Moreover, tables are working...next step is charting the data.

--- app/controllers/userdailydata_controller.rb
+++ app/controllers/userdailydata_controller.rb
@@ -1,31 +1,27 @@
 class UserdailydataController < ApplicationController
-before_filter :authenticate_user!, :except => [:index, :show] 
+before_filter :authenticate_user! # :except => [:index, :show] 
 helper_method :sort_column, :sort_direction
 include ApplicationHelper
 
 ##Need to have this be the data of the specific user##
 def index
- @userdailydata = Userdailydata.all
+ @userdailydata = Userdailydata.where(:user_id=>current_user.id)
 end
 
 def new
-	@userdailydata=Userdailydata.new
-end
-
-def create
-	@userdailydata=Userdailydata.new
-	@userdailydata.save
+@userdailydata=Userdailydata.new
 end
 
 def create
  @userdailydata=Userdailydata.new(allowed_params)
+ @userdailydata.user_id = current_user.id
  @userdailydata.save
 end
 
 #only let this method be accessible within this specific class
 private
 def allowed_params
-  params.require(:userdailydata).permit(:weight,:calories_consumed, :calories_exercised, :date)
+  params.require(:userdailydata).permit(:weight,:calories_consumed, :calories_exercised, :date, :user_id)
 end
 
 
