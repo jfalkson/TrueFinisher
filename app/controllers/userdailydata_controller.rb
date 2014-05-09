68eae69482460fc5688ba348a897eca35923afad
jfalkson <jfalkson@gmail.com>
2014-05-08 22:35:10 -0400

    almost have rickshaw charts working. only item outstanding is expressing the date in integer form (a normalized integer form) so it can be fit on the graph dimensions

--- app/controllers/userdailydata_controller.rb
+++ app/controllers/userdailydata_controller.rb
@@ -6,6 +6,11 @@ include ApplicationHelper
 ##Need to have this be the data of the specific user##
 def index
  @userdailydata = Userdailydata.where(:user_id=>current_user.id)
+ gon.userdailycalories=@userdailydata.pluck(:calories_consumed)
+ gon.userdateorig=@userdailydata.pluck(:date)
+ gon.userdateorig.map! {|d| d.strftime("%Y-%m-%d")}
+ gon.userdate=@userdailydata.pluck(:date)
+ gon.userdate.map! {|d| d.to_i}
 end
 
 def new
