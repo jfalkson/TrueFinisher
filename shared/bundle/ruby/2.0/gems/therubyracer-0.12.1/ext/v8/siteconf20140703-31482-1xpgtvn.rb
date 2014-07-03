6664a7a8cb38a83039cc93db26bc14154c43b477
falkson <jfalkson@gmail.com>
2014-07-03 21:21:54 +0000

    again

--- shared/bundle/ruby/2.0/gems/therubyracer-0.12.1/ext/v8/siteconf20140703-31482-1xpgtvn.rb
+++ shared/bundle/ruby/2.0/gems/therubyracer-0.12.1/ext/v8/siteconf20140703-31482-1xpgtvn.rb
@@ -1,5 +1,5 @@
 require 'rbconfig'
-dest_path = "./.gem.20140703-30310-1y51xzf"
+dest_path = "./.gem.20140703-31482-1aie4nv"
 RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
 RbConfig::CONFIG['sitearchdir'] = dest_path
 RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
